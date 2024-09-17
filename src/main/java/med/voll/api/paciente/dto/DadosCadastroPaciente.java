eed5b8dafd75137c9214308b4db8e882d6b20033
berdnaski <berdnaski1177@gmail.com>
2024-09-17 17:13:13 -0300

    First api pacient

---
+++ src/main/java/med/voll/api/paciente/dto/DadosCadastroPaciente.java
@@ -0,0 +1,10 @@
+package med.voll.api.paciente.dto;
+
+public record DadosCadastroPaciente(
+        String nome,
+        String email,
+        String telefone,
+        String cpf,
+        DadosEndereco endereco
+) {
+}
